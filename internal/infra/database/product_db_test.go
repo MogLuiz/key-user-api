4895532c9e1b938c3fd13522151823e54bcbb118
Luiz Henrique <luizhjramos@outlook.com>
2023-05-27 16:41:00 -0300

    test(e2e): ensure find product by id

--- internal/infra/database/product_db_test.go
+++ internal/infra/database/product_db_test.go
@@ -26,3 +26,24 @@ func Test_E2E_ShouldCreateNewProduct(t *testing.T) {
 	assert.Equal(t, "Product 1", product.Name)
 	assert.Equal(t, 100, product.Price)
 }
+
+func Test_E2E_ShouldFindProductByID(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+
+	product, err := entity.NewProduct("Product 1", 100)
+	assert.Nil(t, err)
+	productDB := NewProduct(db)
+
+	err = productDB.Create(product)
+	assert.NoError(t, err)
+
+	productFound, err := productDB.FindByID(product.ID.String())
+	assert.NoError(t, err)
+	assert.Equal(t, product.ID, productFound.ID)
+	assert.Equal(t, product.Name, productFound.Name)
+	assert.Equal(t, product.Price, productFound.Price)
+}
