2aea53030519d3d659c1e20babe9e2aa6bbf6cd7
Luiz Henrique <luizhjramos@outlook.com>
2023-05-27 17:44:05 -0300

    test(e2e): ensure list products with filter and pagination

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -13,32 +13,32 @@ func Test_ShouldCreateNewProduct(t *testing.T) {
 	assert.NotEmpty(t, product.ID)
 	assert.NotEmpty(t, product.CreatedAt)
 	assert.Equal(t, "Luiz Henrique", product.Name)
-	assert.Equal(t, 10, product.Price)
+	assert.Equal(t, 10.00, product.Price)
 }
 
 func Test_ShouldThrowEmptyNameError(t *testing.T) {
-	product, err := NewProduct("", 10)
+	product, err := NewProduct("", 10.00)
 	assert.NotNil(t, err)
 	assert.Nil(t, product)
 	assert.Equal(t, ErrNameIsRequired, err)
 }
 
 func Test_ShouldThrowEmptyPriceError(t *testing.T) {
-	product, err := NewProduct("Luiz Henrique", 0)
+	product, err := NewProduct("Luiz Henrique", 0.00)
 	assert.NotNil(t, err)
 	assert.Nil(t, product)
 	assert.Equal(t, ErrPriceIsRequired, err)
 }
 
 func Test_ShouldThrowInvalidPriceError(t *testing.T) {
-	product, err := NewProduct("Luiz Henrique", -1)
+	product, err := NewProduct("Luiz Henrique", -1.0)
 	assert.NotNil(t, err)
 	assert.Nil(t, product)
 	assert.Equal(t, ErrInvalidPrice, err)
 }
 
 func Test_ShouldCallValidateMethod(t *testing.T) {
-	product, err := NewProduct("Luiz Henrique", 10)
+	product, err := NewProduct("Luiz Henrique", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, product)
 	assert.Nil(t, product.Validate())
--- internal/infra/database/product_db_test.go
+++ internal/infra/database/product_db_test.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/MogLuiz/key-user-api/internal/entity"
@@ -16,7 +18,7 @@ func Test_E2E_ShouldCreateNewProduct(t *testing.T) {
 	}
 	db.AutoMigrate(&entity.Product{})
 
-	product, err := entity.NewProduct("Product 1", 100)
+	product, err := entity.NewProduct("Product 1", 100.00)
 	assert.Nil(t, err)
 	productDB := NewProduct(db)
 
@@ -24,7 +26,7 @@ func Test_E2E_ShouldCreateNewProduct(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotEmpty(t, product.ID)
 	assert.Equal(t, "Product 1", product.Name)
-	assert.Equal(t, 100, product.Price)
+	assert.Equal(t, 100.00, product.Price)
 }
 
 func Test_E2E_ShouldFindProductByID(t *testing.T) {
@@ -34,7 +36,7 @@ func Test_E2E_ShouldFindProductByID(t *testing.T) {
 	}
 	db.AutoMigrate(&entity.Product{})
 
-	product, err := entity.NewProduct("Product 1", 100)
+	product, err := entity.NewProduct("Product 1", 100.00)
 	assert.Nil(t, err)
 	productDB := NewProduct(db)
 
@@ -55,21 +57,21 @@ func Test_E2E_ShouldUpdateProduct(t *testing.T) {
 	}
 	db.AutoMigrate(&entity.Product{})
 
-	product, err := entity.NewProduct("Product 1", 100)
+	product, err := entity.NewProduct("Product 1", 100.00)
 	assert.Nil(t, err)
 	productDB := NewProduct(db)
 	err = productDB.Create(product)
 	assert.NoError(t, err)
 
 	product.Name = "Product 2"
-	product.Price = 200
+	product.Price = 200.00
 	err = productDB.Update(product)
 	assert.NoError(t, err)
 
 	productFound, err := productDB.FindByID(product.ID.String())
 	assert.NoError(t, err)
 	assert.Equal(t, "Product 2", productFound.Name)
-	assert.Equal(t, 200, productFound.Price)
+	assert.Equal(t, 200.00, productFound.Price)
 }
 
 func Test_E2E_ShouldDeleteProduct(t *testing.T) {
@@ -79,7 +81,7 @@ func Test_E2E_ShouldDeleteProduct(t *testing.T) {
 	}
 	db.AutoMigrate(&entity.Product{})
 
-	product, err := entity.NewProduct("Product 1", 100)
+	product, err := entity.NewProduct("Product 1", 100.00)
 	assert.Nil(t, err)
 	productDB := NewProduct(db)
 	err = productDB.Create(product)
@@ -91,4 +93,37 @@ func Test_E2E_ShouldDeleteProduct(t *testing.T) {
 	productFound, err := productDB.FindByID(product.ID.String())
 	assert.Error(t, err)
 	assert.Nil(t, productFound)
+}
+
+func Test_E2E_ShouldFindAllProductsWithPagination(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+
+	for i := 1; i <= 24; i++ {
+		product, err := entity.NewProduct(fmt.Sprintf("Product %d", i), rand.Float64()*100)
+		assert.Nil(t, err)
+		db.Create(product)
+	}
+
+	productDB := NewProduct(db)
+	productsPage1, err := productDB.FindAll(1, 10, "asc")
+	assert.NoError(t, err)
+	assert.Equal(t, 10, len(productsPage1))
+	assert.Equal(t, "Product 1", productsPage1[0].Name)
+	assert.Equal(t, "Product 10", productsPage1[9].Name)
+
+	productsPage2, err := productDB.FindAll(2, 10, "asc")
+	assert.NoError(t, err)
+	assert.Equal(t, 10, len(productsPage2))
+	assert.Equal(t, "Product 11", productsPage2[0].Name)
+	assert.Equal(t, "Product 20", productsPage2[9].Name)
+
+	productsPage3, err := productDB.FindAll(3, 10, "asc")
+	assert.NoError(t, err)
+	assert.Equal(t, 4, len(productsPage3))
+	assert.Equal(t, "Product 21", productsPage3[0].Name)
+	assert.Equal(t, "Product 24", productsPage3[3].Name)
 }
