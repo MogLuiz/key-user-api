c51d1dc6a1e500ddac5795a76f8341b55cc86288
Luiz Henrique <luizhjramos@outlook.com>
2023-05-29 07:09:36 -0300

    feat: add show product handler

--- internal/infra/webserver/handlers/product.go
+++ internal/infra/webserver/handlers/product.go
@@ -7,6 +7,7 @@ import (
 	"github.com/MogLuiz/key-user-api/internal/dto"
 	"github.com/MogLuiz/key-user-api/internal/entity"
 	"github.com/MogLuiz/key-user-api/internal/infra/database"
+	"github.com/go-chi/chi/v5"
 )
 
 type ProductHandler struct {
@@ -37,4 +38,24 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if product == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
 }
