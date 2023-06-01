c502ec7dcf8b20588e3d5d627a58d7516cdf23f2
Luiz Henrique <luizhjramos@outlook.com>
2023-06-01 06:36:20 -0300

    feat: add user GetJWT handler to generate a user JWT auth

--- internal/infra/webserver/handlers/user.go
+++ internal/infra/webserver/handlers/user.go
@@ -3,20 +3,60 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/MogLuiz/key-user-api/internal/dto"
 	"github.com/MogLuiz/key-user-api/internal/entity"
 	"github.com/MogLuiz/key-user-api/internal/infra/database"
+	"github.com/go-chi/jwtauth"
 )
 
 type UserHandler struct {
-	UserDB database.IUser
+	UserDB       database.IUser
+	Jwt          *jwtauth.JWTAuth
+	JwtExpiresIn int
 }
 
 func NewUserHandler(userDB database.IUser) *UserHandler {
 	return &UserHandler{
 		UserDB: userDB,
 	}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, tokenString, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpiresIn)).Unix(),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	accessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: tokenString,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
 
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
