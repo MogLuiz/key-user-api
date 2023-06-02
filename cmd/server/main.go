b2d007a9332d16917b1bceee38236983198c1526
Luiz Henrique <luizhjramos@outlook.com>
2023-06-01 21:26:17 -0300

    refactor: agroup app routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -32,14 +32,19 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
 
-	r.Post("/users", userHandler.Create)
-	r.Post("/users/generate-jwt", userHandler.GetJWT)
+	r.Route("/product", func(r chi.Router) {
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", userHandler.Create)
+		r.Post("/generate-jwt", userHandler.GetJWT)
+	})
 
 	http.ListenAndServe(":8000", r)
 }
