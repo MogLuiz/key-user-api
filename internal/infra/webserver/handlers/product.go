bea36ceea4c4d4d35ab708021cc1a7d47cad78d0
Luiz Henrique <luizhjramos@outlook.com>
2023-06-04 18:56:04 -0300

    docs: add ShowProduct handler swagger documentation

--- internal/infra/webserver/handlers/product.go
+++ internal/infra/webserver/handlers/product.go
@@ -77,6 +77,18 @@ func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(products)
 }
 
+// GetProduct godoc
+// @Summary      Get a product
+// @Description  Get a product
+// @Tags         products
+// @Accept       json
+// @Produce      json
+// @Param        id   path      string  true  "product ID" Format(uuid)
+// @Success      200  {object}  entity.Product
+// @Failure      404
+// @Failure      500  {object}  Error
+// @Router       /products/{id} [get]
+// @Security ApiKeyAuth
 func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 	if id == "" {
